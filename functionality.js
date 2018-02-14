c1b15a9293816251a4e1480c9ce9331fc71e3168
eregar <30888167+eregar@users.noreply.github.com>
2018-02-13 19:03:48 -0600

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -46,4 +46,44 @@ $(document).ready(function(){
       x=2;
     }
   });
+  $("#rightarrow").click(function(){
+    if(x==1){
+    $("#uno").show();
+    $("#dos").hide();
+    $("#tres").hide();
+    x=3;
+    }
+    else if(x==2){
+    $("#uno").hide();
+    $("#dos").show();
+    $("#tres").hide();
+    x=1;
+    }
+    else{
+      $("#uno").hide();
+      $("#dos").hide();
+      $("#tres").show();
+      x=2;
+    }
+  });
+  $("leftarrow").click(function(){
+    if(x==1){
+    $("#im1").show();
+    $("#im2").hide();
+    $("#im3").hide();
+    x=3;
+    }
+    else if(x==2){
+    $("#im1").hide();
+    $("#im2").show();
+    $("#im3").hide();
+    x=1;
+    }
+    else{
+      $("#im1").hide();
+      $("#im2").hide();
+      $("#im3").show();
+      x=2;
+    }
+  });
 });
