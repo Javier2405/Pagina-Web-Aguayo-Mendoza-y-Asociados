3214d37c85d9c23ef7031eac19e6cd0df24c2e85
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2018-02-14 13:55:46 -0600

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -8,9 +8,9 @@ var preguntas=[
   "la 5ta pregunta"
   ];
 var imagenes=[
-  "im1",
-  "im2",
-  "im3"
+  "<img id='im1' src='Articulos/articulo1.png' class='fluid image'>",
+  "<img id='im2' src='Imagenes/Javier1.png' class='fluid image'>",
+  "<img id='im3' src='Imagenes/Javier2.png' class='fluid image'>"
   ];
 
 
