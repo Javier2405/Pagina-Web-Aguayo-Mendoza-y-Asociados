62dccdda1c818dcd689aaa087753a57cd4fc68e8
eregar <30888167+eregar@users.noreply.github.com>
2018-02-14 23:29:41 -0600

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,18 +1,22 @@
 var x=0;
 var y=0;
 var preguntas=[
-  "¿Debes presentar informativa de subsidio al empleo entregado y disminuido?",
-  "¿ Cuando es obligatorio emitir en complemento de pago del CFDI 3.3?",
-  "¿A partir de que fecha debe solicitarse autorización al cliente para poder cancelar un CFDI 3.3?",
-  "¿ Si anotas como método de pago PPD Pago en parcialidades o diferido puedes utilizar como Forma de Pago una diferente a 99 Por Definir?",
-  "¿Debes utilizar para el adecuado manejo de CFDI de anticipos un CFDI de Egresos?"
+  "debes presentar informativa de subsidio al empleo entregado y disminuido?",
+  "Cuando es obligatorio emitir en complemento de pago del CFDI 3.3?",
+  "A partir de que fecha debe solicitarse autorizacion al cliente para poder cancelar un CDFI 3.3?",
+  "Si anotas como metodo de pago PPD Pago en parcialidades o diferido puedes utilizar como forma de pago una diferente a 99 por definir?",
+  "Debes utilizar el adecuado manejo de CFDI de anticipos o un CFDI de egresos?",
+  "En que medio pueden almacenarse los xml emitidos y recibidos? ",
+  "que requisitos deben reunir las presentaciones impresas del CFDI?"
   ];
 var respuestas=[
-  "NO, el Sat de ultimo momento ha confirmado que no es necesario",
-  "A partir del 1 de Abril de 2018",
-  "A partir del 1 de Julio de 2018",
-  "NO, siempre que se utilice PPD debe anotarse como forma de pago 99 Por Definir",
-  "Si es importante tener en cuenta que en general debes emitir un CFDI de Ingresos para anticipo, un segundo de Ingresos para la operación completa y uno de Egresos para “ajustar” el anticipo."
+  "No, el SAT de ultimo momento ha confirmado que no es necesario",
+  "A partir del 1ro de abril de 2018",
+  "A partir del 1ro de julio de 2018",
+  "NO, siempre que se utilice PPD debe anotarse como forma de pago 99 por definir",
+  "Si es importante tener en cuenta que en general debes de emitir un CFDI de ingresos para anticipo, un segundo de ingresos para la operacion completa y uno de egresos para ajustar el anticipo",
+  "Deberan almacenarlo en medios magneticos, opticos o de cualquier otra tecnologia",
+  "los señalados en los articulos 29,29 A del CFF y regla 2.7.1.7"
 ];
 // cuando pongas comitas dentro de otras comitas hay que usar ' en vez de " para no interferir
 var imagenes=[
@@ -29,13 +33,13 @@ $(document).ready(function(){
   
   $("#prev").click(function(){
     //cambiar mods a la cantidad de preguntas del array
-    x=(x+1)%5;
+    x=(x+1)%7;
     document.getElementById("uno").innerHTML=preguntas[x];
     document.getElementById("dos").innerHTML=respuestas[x];
   });
   $("#next").click(function(){
     //cambiar mods a la cantidad de preguntas del array YYY cambiar el 4 a la cantidad -1
-    x=(x+4)%5;
+    x=(x+6)%7;
     document.getElementById("uno").innerHTML=preguntas[x];
     document.getElementById("dos").innerHTML=respuestas[x];
   });
