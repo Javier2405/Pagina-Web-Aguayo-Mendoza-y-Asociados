41f751f563da0d9d511316587bc33304d2fd94f0
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2018-02-14 23:24:40 -0600

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,23 +1,23 @@
 var x=0;
 var y=0;
 var preguntas=[
-  "una pregunta",
-  "otra pregunta",
-  "la tercera pregunta",
-  "la 4ta pregunta",
-  "la 5ta pregunta"
+  "¿Debes presentar informativa de subsidio al empleo entregado y disminuido?",
+  "¿ Cuando es obligatorio emitir en complemento de pago del CFDI 3.3?",
+  "¿A partir de que fecha debe solicitarse autorización al cliente para poder cancelar un CFDI 3.3?",
+  "¿ Si anotas como método de pago PPD Pago en parcialidades o diferido puedes utilizar como Forma de Pago una diferente a 99 Por Definir?",
+  "¿Debes utilizar para el adecuado manejo de CFDI de anticipos un CFDI de Egresos?"
   ];
 var respuestas=[
-  "una respuesta",
-  "otra respuesta",
-  "la tercera respuesta",
-  "la cuarta respuesta",
-  "la quinta respuesta"
+  "NO, el Sat de ultimo momento ha confirmado que no es necesario",
+  "A partir del 1 de Abril de 2018",
+  "A partir del 1 de Julio de 2018",
+  "NO, siempre que se utilice PPD debe anotarse como forma de pago 99 Por Definir",
+  "Si es importante tener en cuenta que en general debes emitir un CFDI de Ingresos para anticipo, un segundo de Ingresos para la operación completa y uno de Egresos para “ajustar” el anticipo."
 ];
 // cuando pongas comitas dentro de otras comitas hay que usar ' en vez de " para no interferir
 var imagenes=[
   "<a  href='http://www.eluniversal.com.mx/cartera/finanzas/enterate-10-practicas-indebidas-en-la-e-factura-que-el-sat-detecto' target='_blank'><img id='im1' src='Articulos/articulo1.png' class='fluid image'></a>",
-  "<a  href='Fiscalizacion.docx' target='_blank'><img id='im2' src='Articulos/articulo1.png' class='fluid image'></a>",
+  "<a  href='Fiscalizacion.docx' target='_blank'><img id='im2' src='Articulos/art2.png' class='fluid image'></a>",
   "<a  href='Riesgos.docx' target='_blank'><img id='im3' src='Articulos/art3.png' class='fluid image'></a>"
   ];
 
