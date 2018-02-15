b04c945fd23202a00662ccd7a524da285ffaec37
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2018-02-14 23:06:30 -0600

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -16,7 +16,7 @@ var respuestas=[
 ];
 // cuando pongas comitas dentro de otras comitas hay que usar ' en vez de " para no interferir
 var imagenes=[
-  "<img id='im1' href='www.google.com' src='Articulos/articulo1.png' class='fluid image'>",
+  "<a  href='www.google.com'><img id='im1' src='Articulos/articulo1.png' class='fluid image'></a>",
   "<img id='im2' src='Imagenes/Javier1.png' class='fluid image'>",
   "<img id='im3' src='Imagenes/Javier2.png' class='fluid image'>"
   ];
