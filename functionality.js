661fb4505da497147c5fcfafed7d6a2424ab7797
eregar <30888167+eregar@users.noreply.github.com>
2018-02-14 18:20:16 -0600

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -7,6 +7,13 @@ var preguntas=[
   "la 4ta pregunta",
   "la 5ta pregunta"
   ];
+var respuestas=[
+  "una respuesta",
+  "otra respuesta",
+  "la tercera respuesta",
+  "la cuarta respuesta",
+  "la quinta respuesta"
+];
 // cuando pongas comitas dentro de otras comitas hay que usar ' en vez de " para no interferir
 var imagenes=[
   "<img id='im1' href='www.google.com' src='Articulos/articulo1.png' class='fluid image'>",
@@ -18,16 +25,19 @@ var imagenes=[
 $(document).ready(function(){
   document.getElementById("uno").innerHTML=preguntas[0];
   document.getElementById("imash").innerHTML=imagenes[0];
+  document.getElementById("dos").innerHTML=respuestas[0];
   
   $("#prev").click(function(){
     //cambiar mods a la cantidad de preguntas del array
     x=(x+1)%5;
     document.getElementById("uno").innerHTML=preguntas[x];
+    document.getElementById("dos").innerHTML=respuestas[x];
   });
   $("#next").click(function(){
     //cambiar mods a la cantidad de preguntas del array YYY cambiar el 4 a la cantidad -1
     x=(x+4)%5;
     document.getElementById("uno").innerHTML=preguntas[x];
+    document.getElementById("dos").innerHTML=respuestas[x];
   });
   $("#rightarrow").click(function(){
     y=(y+1)%3;
